773dab923edc281202f63beb3eeefc48faa2e2fd
Kevin Thompson <kevin@kevinthompson.info>
2018-01-12 12:33:38 -0800

    Fix phrasing

--- lib/fashion_fairy/forecast.rb
+++ lib/fashion_fairy/forecast.rb
@@ -27,7 +27,7 @@ module FashionFairy
 
     def to_s
       %(
-        Right now in #{city} it's #{current.temp} degrees with #{current.text}.
+        Right now in #{city} it's #{current.temp} degrees and #{current.text}.
         Today's forecast is #{today.text} with a high of #{today.high} degrees
         and a low of #{today.low} degrees.
       )
